938ccb81f693197edb49ac76b69f6d82ca345668
Juanjo <juanjose.ortizgarcia@outlook.com>
2024-04-24 17:54:04 +0200

    add homelayout and routes

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-
+    HomeLayoutComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  exports:[]
+  exports:[NavbarComponent]
 })
 export class SharedModule { }
